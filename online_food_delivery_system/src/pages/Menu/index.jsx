e4f504411bb5d355a82f9d592e67d828ed7334e6
Hayagreevan V <hayagree@gmail.com>
2023-04-28 13:56:59 +0530

    Update index.jsx

--- online_food_delivery_system/src/pages/Menu/index.jsx
+++ online_food_delivery_system/src/pages/Menu/index.jsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {fetchProducts, selectAllProducts } from '../../stores/menu/productsSlice';
-=======
-import {useEffect} from "react";
-import {useDispatch,useSelector} from "react-redux";
-//import {fetchProducts,selectAllProducts} from 
 
->>>>>>> c2e7a9e859b0743b7e17f63d5d13d0af5d197ab5
 
 const Menu=() =>{
     const dispatch = useDispatch();
