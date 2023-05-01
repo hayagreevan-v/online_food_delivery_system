08ecc6ef7076a9cc9219e6cd6e8e5d549af5e2ed
Keerthana J <keerthanajothirameshkumar@gmail.com>
2023-05-01 21:54:11 +0530

    Homepage update

--- online_food_delivery_system/src/components/About.jsx
+++ online_food_delivery_system/src/components/About.jsx
@@ -8,8 +8,13 @@ export const About = () => {
                 <div className="">
                     <h2 className="text-2xl font-medium">About Us</h2>
                     <p className="text-lg">
-                    Online Food Delivery System created using MERN (MongoDB, ExpressJs, ReactJs, NodeJs) 
-                        for CS19443 Database Management System Project.<br/>
+                    <br></br>
+                    <strong>CS19443 Database Management System Project.</strong><br></br>
+                    Our project is a digital platform that allows customers to place orders for food from various restaurants or food establishments through the internet. The system typically consists of a website and mobile app that enables customers to browse menus, choose items, and pay for their orders. The common features available in our project is menu management, order management, payment processing, delivery management and customer management. Additionally the users can also give their feedback and reviews through our app.
+                    The project was built and deployed using  
+                         <strong> MERN Stack(React for Front end and MongoDB,Express and Node JS for Back end)</strong><br/>
+                         <br></br>
+                         Developed by
                         Team B6 - <strong>Keerthana J (210701118)</strong>  and <strong>Hayagreevan V (210701080)</strong> 
                     </p>
                 </div>
