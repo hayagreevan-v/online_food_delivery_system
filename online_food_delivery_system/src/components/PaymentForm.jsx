f897577aa8fa16c7786d73b35cb3bfdde171901a
Hayagreevan V <hayagree@gmail.com>
2023-05-01 21:48:53 +0530

    bug fix

--- online_food_delivery_system/src/components/PaymentForm.jsx
+++ online_food_delivery_system/src/components/PaymentForm.jsx
@@ -96,7 +96,7 @@ const handleSubmit = (e) => {
     const orderData = {
         //paymentMethodType: 'card',
         orderItems: cart,
-        user: 'Hayagreevan',
+        user: sessionStorage.getItem('User Id'),
         shippingAddress: address,
     };
     createOrder(orderData);
